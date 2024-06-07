b86a290a4d94723f420194601a865e5c9eb0f266
niko <niko@velvet.moe>
2024-06-07 02:45:27 -0700

    fix: rollup magic

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,6 +2,8 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
+import './src/drizzle/meta/_journal.json';
+
 const migrationClient = postgres(process.env.POSTGRES_URL!, { max: 1});
 migrate(drizzle(migrationClient), { migrationsFolder: './src/drizzle' });
 
