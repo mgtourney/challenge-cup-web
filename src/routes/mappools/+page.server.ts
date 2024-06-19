5a647351853071f52403db34715de2b59d92c306
niko <niko@velvet.moe>
2024-06-18 23:07:01 -0700

    fix: ratelimiting pt2

--- src/routes/mappools/+page.server.ts
+++ src/routes/mappools/+page.server.ts
@@ -11,25 +11,31 @@ export async function load() {
     }).from(mappools);
 
     let promise = m.map(async (mappool) => {
+        //random delay
+        let delay = Math.floor(Math.random() * 300);
 
-        let promise = mappool.map_ids.map(async (id) => {
-            let map = await db.select({
+        let our_maps: any[] = mappool.map_ids.map(id => {
+            let map: any = db.select({
                 hex: maps.hex,
                 diff: maps.diff,
             }).from(maps).where(eq(maps.id, id)).limit(1);
+            return map[0];
+        })
 
-            // add a random cool down to prevent rate limiting
-            await new Promise(resolve => setTimeout(resolve, Math.random() * 100));
+        let comma_separated = our_maps.map(map => map.hex).join(',');
 
-            let res = await fetch(`https://api.beatsaver.com/maps/id/${map[0].hex}`);
-            if (res.ok) {
-                let data = await res.json();
-                return {name: data.metadata.songName, author: data.metadata.songAuthorName, icon: data.versions[data.versions.length - 1].coverURL, mapper: data.metadata.levelAuthorName, diff: map[0].diff, hex: map[0].hex};
-            } else {
-                console.error(`Failed to fetch map ${map[0].hex}`);
-                console.log(await res.text());
-                return {name: "Unknown", author: "Unknown", mapper: "Unknown", icon: "https://placehold.co/400", diff: "Unknown", hex: map[0].hex};
-            }
+        let map_data = await fetch(`https://api.beatsaver.com/maps/ids/${comma_separated}`);
+
+        if (!map_data.ok) {
+            console.error(`Failed to fetch map ${comma_separated}`);
+            console.log(await map_data.text());
+            return {name: "Unknown", author: "Unknown", mapper: "Unknown", icon: "https://placehold.co/400", diff: "Unknown", hex: comma_separated};
+        }
+
+        let map_json = await map_data.json();
+
+        let promise = map_json.docs.map(async (data: any, index: number) => {
+            return {name: data.metadata.songName, author: data.metadata.songAuthorName, icon: data.versions[data.versions.length - 1].coverURL, mapper: data.metadata.levelAuthorName, diff: our_maps[index].diff, hex: our_maps[index].hex};
         });
 
         let mp = <{name: string, description: string, bplist: string, maps: {name: string, author: string, icon: string, mapper: string, diff: string, hex: string}[]}><unknown>mappool;
